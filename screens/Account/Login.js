755e80f7d276fcc60551cfb0989c7ea63d284189
ShenLong03 <47692181+ShenLong03@users.noreply.github.com>
2021-03-21 20:12:51 -0600

    Cambios en Login

--- screens/Account/Login.js
+++ screens/Account/Login.js
@@ -1,14 +1,59 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from "react";
+import { Text } from "react-native";
+import { View } from "react-native";
+import { Image, ScrollView, StyleSheet } from "react-native";
+import { Divider } from "react-native-elements";
 
 export default class Login extends Component {
-    render() {
-        return (
-            <View>
-                <Text> LOGIN </Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <ScrollView>
+        <Image
+          source={require("../../assets/punto-coma.png")}
+          resizeMode="contain"
+          style={styles.image}
+        />
+        <View style={styles.container}>
+          <Text>Login Form</Text>
+          <CreateAccount />
+        </View>
+        <Divider style={styles.divider} />
+      </ScrollView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({})
+function CreateAccount(props) {
+  return (
+    <Text style={styles.register}
+    onPress={()=>{console.log("Register")}}
+    >
+      ¿Aun no tienes una cuenta?{" "}
+      <Text style={styles.btnRegister}>Regístrate</Text>
+    </Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    height: 150,
+    width: "100%",
+    marginBottom: 20,
+  },
+  container: {
+    marginHorizontal: 40,
+  },
+  divider: {
+    backgroundColor: "#b60073",
+    margin: 40,
+  },
+  register:{
+      marginTop:15,
+      marginHorizontal:10,
+      alignSelf:"center"
+  },
+  btnRegister:{
+      color:"#b60073",
+      fontWeight:"bold"
+  }
+});
